10628ef5daca2a176de7f0d2746af41259205723
Dady Nasser 89 <dadyutenga@yahoo.com>
2025-08-06 12:51:29 +0300

    done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,6 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-# For production:
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
